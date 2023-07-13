327622fdaf3fd2e018b48e0b97e3faaea28d7374
jaiadithyan-06 <96172866+jaiadithyan-06@users.noreply.github.com>
2023-07-13 11:43:39 +0530

    Add files via upload

---
+++ Day 4 Java/All Digits Count.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import  java.util.*;
+
+class UserMainCode {
+
+    public int AllDigitsCount (int input1) {
+
+        while(input1>0){     
+            r=input1%10;       
+            c++;       
+        input1=input1/10;     
+        }     
+        return c;   
+
+
+    }
+}
