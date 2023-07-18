cfcb818b35ec28882ca129c77759cbf8300cecf1
jaiadithyan-06 <96172866+jaiadithyan-06@users.noreply.github.com>
2023-07-18 12:05:00 +0530

    Add files via upload

---
+++ Day 7 Java/second word upper.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import  java.util.*;
+
+class UserMainCode {
+
+    public String secondWordUpperCse (String input1) {
+
+         String s[]=input1.split(" ");
+        if(s.length==1)
+        return "LESS";
+        String s1=s[1];
+        s1=s1.toUpperCase();
+        return s1;
+
+    }
+}
+ 
